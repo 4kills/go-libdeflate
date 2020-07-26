d7dce008a526ed5c0f801275c12c3966aa15d40f
4kills <4kills@4killss-iMac.local>
2020-07-26 09:19:25 -0700

    added support for macos and tested it

--- native/cgo.go
+++ native/cgo.go
@@ -4,5 +4,6 @@ package native
 #cgo CFLAGS: -I${SRCDIR}/libs/
 #cgo windows LDFLAGS: ${SRCDIR}/libs/libdeflatewin.a
 #cgo linux LDFLAGS: ${SRCDIR}/libs/libdeflatelinux.a
+#cgo darwin LDFLAGS: ${SRCDIR}/libs/libdeflatedarwin.a
 */
 import "C"
