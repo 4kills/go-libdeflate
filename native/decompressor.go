c49ad8ff253819ebdac75b81495183115482329f
4kills <dominik.ochs@gmx.net>
2020-07-26 17:18:02 +0200

    added testing and fixed a inverted if statement

--- native/decompressor.go
+++ native/decompressor.go
@@ -3,9 +3,16 @@ package native
 /*
 #include "libdeflate.h"
 #include "helper.h"
+#include <stddef.h>
+#include <stdlib.h>
 
 typedef struct libdeflate_decompressor decomp;
 typedef enum libdeflate_result res;
+
+size_t* sizePtr(int fit) {
+	if (fit == 1) return NULL;
+	return (size_t*) malloc(sizeof(size_t));
+}
 */
 import "C"
 import "unsafe"
@@ -25,22 +32,60 @@ func NewDecompressor() (*Decompressor, error) {
 	return &Decompressor{dc}, nil
 }
 
-// Decompress decompresses the given data from in to out and returns the number of bytes
-// written to out, out and an error if something went wrong.
-// If error != nil then the data in out is undefined.
-// If you pass nil as out this function will allocate a sufficient buffer.
+// Decompress decompresses the given data from in to out and returns out and an error if something went wrong.
+// If error != nil, then the data in out is undefined.
+// If you pass a buffer to out, the size of this buffer must exactly match the length of the decompressed data.
+// If you pass nil as out, this function will allocate a sufficient buffer and return it.
 func (dc *Decompressor) Decompress(in, out []byte) ([]byte, error) {
 	if len(in) == 0 {
 		return out, errorNoInput
 	}
 
-	panic("implement")
-	return nil, nil
+	if out != nil {
+		_, err := dc.decompress(in, out, true)
+		return out, err
+	}
+
+	n := 0
+	inc := 6
+	err := errorInsufficientSpace
+	for err == errorInsufficientSpace {
+		out = make([]byte, len(in)*inc)
+		n, err = dc.decompress(in, out, false)
+		if inc >= 16 {
+			inc += 3
+			continue
+		}
+		inc += 5
+	}
+
+	return out[:n], err
 }
 
-func (dc *Decompressor) decompress(in, out []byte) ([]byte, error) {
+func (dc *Decompressor) decompress(in, out []byte, fit bool) (int, error) {
+	inAddr := startMemAddr(in)
+	outAddr := startMemAddr(out)
 
-	return nil, nil
+	f := 0
+	if fit {
+		f = 1
+	}
+	s := C.sizePtr(C.int(f))
+
+	r := C.libdeflate_zlib_decompress(dc.dc,
+		unsafe.Pointer(inAddr), intToInt64(len(in)),
+		unsafe.Pointer(outAddr), intToInt64(len(out)),
+		s,
+	)
+	err := parseResult(C.res(r))
+
+	n := len(out)
+	if !fit {
+		n = int(*s)
+	}
+	C.free(unsafe.Pointer(s))
+
+	return n, err
 }
 
 func parseResult(r C.res) error {
--- native/decompressor_test.go
+++ native/decompressor_test.go
@@ -1,1 +1,44 @@
 package native
+
+import (
+	"bytes"
+	"compress/zlib"
+	"testing"
+)
+
+func TestParseResult(t *testing.T) {
+	if err := parseResult(1); err != errorBadData {
+		t.Fail()
+	}
+	if err := parseResult(200); err != errorUnknown {
+		t.Fail()
+	}
+	if err := parseResult(0); err != nil {
+		t.Fail()
+	}
+}
+
+func TestDecompress(t *testing.T) {
+	// compress with go standard zlib
+	buf := &bytes.Buffer{}
+	w := zlib.NewWriter(buf)
+	w.Write([]byte(shortString))
+	w.Close()
+	in := buf.Bytes()
+
+	// decompress with this lib
+
+	out := make([]byte, len(shortString))
+	dc, _ := NewDecompressor()
+	defer dc.Close()
+	if _, err := dc.Decompress(in, out); err != nil {
+		t.Error(err)
+	}
+	slicesEqual([]byte(shortString), out, t)
+
+	out, err := dc.Decompress(in, nil)
+	if err != nil {
+		t.Error(err)
+	}
+	slicesEqual([]byte(shortString), out, t)
+}
