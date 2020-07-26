2cda7614fcbed949ec6ee54ab159dea4d94de75c
4kills <dominik.ochs@gmx.net>
2020-07-26 20:31:38 +0200

    reformatted errors

--- native/errors.go
+++ native/errors.go
@@ -3,13 +3,13 @@ package native
 import "errors"
 
 var (
-	errorOutOfMemory  = errors.New("libdeflate: native: out of memory")
-	errorInvalidLevel = errors.New("libdeflate: native: illegal compression level")
-	errorShortBuffer  = errors.New("libdeflate: native: short buffer")
-	errorNoInput      = errors.New("libdeflate: native: empty input")
-	errorBadData      = errors.New("libdeflate: native: bad data: data was corrupted, invalid or unsupported")
-	errorUnknown      = errors.New("libdeflate: native: unknown error code from c library")
-	errorShortOutput  = errors.New("libdeflate: native: buffer too long: decompressed to fewer bytes than expected, indicating possible error in decompression. Make sure your out buffer has the exact length of the decompressed data or pass nil for out")
+	errorOutOfMemory   = errors.New("libdeflate: native: out of memory")
+	errorInvalidLevel  = errors.New("libdeflate: native: illegal compression level")
+	errorShortBuffer   = errors.New("libdeflate: native: short buffer")
+	errorNoInput       = errors.New("libdeflate: native: empty input")
+	errorBadData       = errors.New("libdeflate: native: bad data: data was corrupted, invalid or unsupported")
+	errorUnknown       = errors.New("libdeflate: native: unknown error code from c library")
+	errorShortOutput   = errors.New("libdeflate: native: buffer too long: decompressed to fewer bytes than expected, indicating possible error in decompression. Make sure your out buffer has the exact length of the decompressed data or pass nil for out")
 	errorAlreadyClosed = errors.New("libdeflate: native: (de-)compressor already closed. It must not be used anymore")
 
 	// checked error (in native)
