bdce500256c7a2512678690ef9341f53bd6af312
4kills <dominik.ochs@gmx.net>
2020-07-30 14:47:06 +0200

    added adler32 and crc32 checksum

--- native/checksum.go
+++ native/checksum.go
@@ -30,7 +30,7 @@ func Adler32(adler32 uint32, in []byte) uint32 {
 Crc32 works as described in native/libs/libdeflate.h.
 Returns 0 for in == nil
 */
-func Crc32(adler32 uint32, in []byte) uint32 {
+func Crc32(crc32 uint32, in []byte) uint32 {
 	if in == nil {
 		return 0
 	}
@@ -38,7 +38,7 @@ func Crc32(adler32 uint32, in []byte) uint32 {
 	addr := startMemAddr(in)
 
 	checksum := C.libdeflate_crc32(
-		toUInt32(adler32),
+		toUInt32(crc32),
 		unsafe.Pointer(addr),
 		toInt64(int64(len(in))),
 	)
